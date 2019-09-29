2351d82a3151cb2ab36b8f4f04f98c1bd12f920e
JonathanGWesterfield <jgwesterfield@gmail.com>
2019-09-29 17:17:27 -0500

    Added models for the Item, Member_Position, and Order_Transaction Tables

--- Inventory-Site/app/User.php
+++ Inventory-Site/app/User.php
@@ -36,4 +36,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $table = 'users'; // table name
+    protected $primaryKey = 'id'; // primary key
+    protected $timestamps = true; // timestamps
 }
