06ee96374dadcc2d0f9a0b7e5797d597e2bb2a4f
JonathanGWesterfield <jgwesterfield@gmail.com>
2019-10-04 22:09:05 -0500

    Setup the database relationships in the models

--- Inventory-Site/app/Transaction.php
+++ Inventory-Site/app/Transaction.php
@@ -9,4 +9,13 @@ class Transaction extends Model
     protected $table = 'Order_Transaction'; // table name
     protected $primaryKey = 'id'; // primary key
     public $timestamps = true; // timestamps
+
+    public function Transaction()
+    {
+        // Define the table relationships
+        $this->belongsTo(User::Class);
+        $this->belongsTo(Item::Class);
+    }
+
+
 }
