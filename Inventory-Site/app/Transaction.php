f0213487e570772d02b2c3fe37f81feaeac3e7c6
JonathanGWesterfield <jgwesterfield@gmail.com>
2019-10-06 15:50:39 -0500

    Added function to allow getting current quantity for an item

--- Inventory-Site/app/Transaction.php
+++ Inventory-Site/app/Transaction.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Transaction extends Model
 {
@@ -15,6 +16,20 @@ class Transaction extends Model
         // Define the table relationships
         $this->belongsTo(User::Class);
         $this->belongsTo(Item::Class);
+    }
+
+    /**
+     * Allows us to get the current quantity for an item in the database.
+     *
+     * @param $itemId The id of the item we are trying to get the current quantity for.
+     */
+    public function quantity($itemId)
+    {
+        $quantity = DB::table('Order_Transaction')
+            ->sum('item_quantity_change')
+            ->where('item_id', $itemId);
+
+        return $quantity;
     }
 
 
