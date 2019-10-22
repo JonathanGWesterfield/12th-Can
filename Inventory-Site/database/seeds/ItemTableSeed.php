cf2b0409c42250d61fed76650581987142b53438
JonathanGWesterfield <jgwesterfield@gmail.com>
2019-10-22 15:30:33 -0500

    Fixed all of the database seeds

--- Inventory-Site/database/seeds/ItemTableSeed.php
+++ Inventory-Site/database/seeds/ItemTableSeed.php
@@ -15,11 +15,23 @@ class ItemTableSeed extends Seeder
             'name' => 'Yeeterinos',
             'capacity' => 420,
             'quantity'=> 0,
-            'threshold' => 42,
-            'isFood' => 'true',
+            'low_threshold' => 42,
+            'is_food' => 'true',
             'refrigerated' => 'false',
             'created_at' => '2019-10-15 18:01:17',
             'updated_at' => '2019-10-15 18:01:17',
+            'removed' => false
+        ]);
+
+        DB::table('Item')->insert([
+            'name' => 'Yeeteronis',
+            'capacity' => 4200,
+            'quantity'=> 0,
+            'low_threshold' => 420,
+            'is_food' => 'true',
+            'refrigerated' => 'true',
+            'created_at' => '2019-10-16 18:01:17',
+            'updated_at' => '2019-10-16 18:01:17',
             'removed' => false
         ]);
     }
