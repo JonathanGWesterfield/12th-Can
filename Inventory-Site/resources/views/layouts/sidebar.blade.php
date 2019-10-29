aa017acb0d04d1f9f03ec4028618488a8bfb2ba4
Ismael Rodriguez <ir3987@tamu.edu>
2019-10-29 10:34:07 -0500

    Safari fix?

--- Inventory-Site/resources/views/layouts/sidebar.blade.php
+++ Inventory-Site/resources/views/layouts/sidebar.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="{{ csrf_token() }}">
 
     <title>The 12th Can</title>
-
+    <link href="{{ asset('css/Normalize.css') }}" rel="stylesheet">
     <!-- Bootstrap CSS CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     <!-- Our Custom CSS -->
