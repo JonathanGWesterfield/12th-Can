fb8fe7e538bd5b12e78eaf658d1562fe5e626a01
JonathanGWesterfield <jgwesterfield@gmail.com>
2019-10-29 08:41:09 -0500

    Fixed the OrderTransactionSeeder file

--- Inventory-Site/database/seeds/OrderTransactionTableSeeder.php
+++ Inventory-Site/database/seeds/OrderTransactionTableSeeder.php
@@ -12,36 +12,32 @@ class OrderTransactionTableSeeder extends Seeder
     public function run()
     {
         DB::table('Order_Transaction')->insert([
-                   'id' => 1,
-                   'member_id' => 7,
-                   'item_id'=> 2,
-                   'item_quantity_change' => 69,
-                   'comment' => 'Nice',
-                   'transaction_date' => '2019-10-16 18:01:17'
-               ]);
-        
+            'member_id' => 1,
+            'item_id'=> 1,
+            'item_quantity_change' => 69,
+            'comment' => 'Nice',
+            'transaction_date' => '2019-10-16 18:01:17'
+        ]);
+
         DB::table('Order_Transaction')->insert([
-            'id' => 2,
-            'member_id' => 7,
+            'member_id' => 1,
             'item_id'=> 2,
             'item_quantity_change' => -69,
             'comment' => 'Nice',
             'transaction_date' => '2019-10-16 18:01:17'
         ]);
-        
+
         DB::table('Order_Transaction')->insert([
-            'id' => 3,
-            'member_id' => 7,
+            'member_id' => 1,
             'item_id'=> 2,
             'item_quantity_change' => 69,
             'comment' => 'Nice',
             'transaction_date' => '2019-10-16 18:01:17'
         ]);
-        
+
         DB::table('Order_Transaction')->insert([
-            'id' => 4,
-            'member_id' => 7,
-            'item_id'=> 3,
+            'member_id' => 1,
+            'item_id'=> 1,
             'item_quantity_change' => 69,
             'comment' => 'Nice',
             'transaction_date' => '2019-10-16 18:01:17'
