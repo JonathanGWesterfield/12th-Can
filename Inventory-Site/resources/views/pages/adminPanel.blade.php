f697924cf559ca65c16c46bfbb01a6675d15983d
Mannan <mannanmendiratta99@gmail.com>
2019-11-14 09:22:16 -0600

    Modify Account Modal

--- Inventory-Site/resources/views/pages/adminPanel.blade.php
+++ Inventory-Site/resources/views/pages/adminPanel.blade.php
@@ -8,6 +8,50 @@
 @inject('Item', 'App\Item')
 <meta name="csrf-token" content="{{ csrf_token() }}" />
 <div ng-app="admin" ng-controller="adminPanel">
+<div class="modal fade" id="modifyAccModal" tabindex="-1" role="dialog" aria-labelledby="modifyAccModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modifyAccModalLabel">Modify Account</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+            </div>
+            <div class="modal-body">
+                <form ng-submit = "modifyAcc()">
+                    <div class="form-row">
+                        <label for="accName">Name</label>
+                        <input ng-modal = "accNameVal" value = "<%accNameVal%>" type="text" class="form-control" id="accName" required>
+                    </div>
+                    <div class="form-row">
+                        <label for="accEmail">Email</label>
+                        <input ng-modal = "accEmailVal" value = "<%accEmailVal%>" type="email" class="form-control" id="accEmail" required>
+                    </div>
+                    <div class="form-row">
+                        <label for="accPhone">Phone Number</label>
+                        <input ng-modal = "accPhoneVal" value = "<%accPhoneVal%>" type="text" class="form-control" id="accPhone" required>
+                    </div>
+                    <div class="form-group">
+                        <div class="form-check">
+                            <input ng-modal = "accArcVal" ng-checked = "accArcVal" type="checkbox" class="form-check-input" id="accArchive">
+                            <label class="form-check-label" for="accArchive">Archive Account?</label>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="form-row">
+                            <select ng-model="accPosVal">
+                                <option ng-repeat="x in currentPos" value = "<%x.position%>"><%x.position%></option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-row" style="float:right">
+                        <button class="btn btn-primary" type="submit">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
     <div class="row">
         <div class="col" style="text-align: center">
             <h2>Admin Panel</h2>
@@ -121,29 +165,38 @@
             $scope.pastAcc = [];
             $scope.pendingAcc = [];
             $scope.currentPos = [];
-
+            $scope.accNameVal = "3ads";
+            $scope.accPhoneVal = "";
+            $scope.accEmailVal = "";
+            $scope.accArcVal = false;
+            $scope.currentMod = false;
+            $scope.index = index;
             //Over here do get calls to get evrything from the admin panel
             //Adding random values to showcase evrything rn
             $scope.currentAcc = [
             {
                 'name': 'Mike',
                 'phone': '(123)-456-7890',
-                'email': 'abc@mail.com'
+                'email': 'abc@mail.com',
+                'position': 'Exec'
             },
             {
                 'name': 'Mike2',
                 'phone': '(122)-456-7890',
-                'email': 'abcd@mail.com'
+                'email': 'abcd@mail.com',
+                'position': 'Exec'
             },
             {
                 'name': 'Mike3',
                 'phone': '(124)-456-7890',
-                'email': 'abce@mail.com'
+                'email': 'abce@mail.com',
+                'position': 'Exec'
             },
             {
                 'name': 'Mike4',
                 'phone': '(125)-456-7890',
-                'email': 'abcf@mail.com'
+                'email': 'abcf@mail.com',
+                'position': 'Exec'
             }]
 
             $scope.pastAcc = [
@@ -213,11 +266,27 @@
 
         })
         $scope.modifyCurrent = function(index){
-
+            $scope.currentMod = true;
+            account = $scope.currentAcc[index]
+            $scope.accNameVal = account.name;
+            $scope.accPhoneVal = account.phone;
+            $scope.accEmailVal = account.email;
+            $scope.accPosVal = account.position;
+            $scope.accArcVal = false;
+            $scope.index = index;
+            $('#modifyAccModal').modal('show');
         }
 
         $scope.modifyPast = function(index){
-
+            $scope.currentMod = false;
+            account = $scope.pastAcc[index]
+            $scope.accNameVal = account.name;
+            $scope.accNameVal = account.phone;
+            $scope.accNameVal = account.email;
+            $scope.accPosVal = "";
+            $scope.accArcVal = true;
+            $scope.index = index;
+            $('#modifyAccModal').modal('show');
         }
 
         $scope.acceptAcc = function(index){
@@ -229,6 +298,12 @@
         }
 
         $scope.modifyPos = function(index){
+            
+        }
+
+        $scope.modifyAcc = function(){
+            console.log($scope.accEmailVal);
+            console.log($scope.accPosVal);
             
         }
     })
