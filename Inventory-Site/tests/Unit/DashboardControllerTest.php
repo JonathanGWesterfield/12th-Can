796ee3a0c19d3f0054fe7c662bd9103a0b5b8589
Aaron todd <altodd@Aarons-MacBook-Pro-3.local>
2019-11-21 10:38:45 -0600

    Added unit test for dashboard

---
+++ Inventory-Site/tests/Unit/DashboardControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DashboardControllerTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $this->assertTrue(true);
+    }
+}
